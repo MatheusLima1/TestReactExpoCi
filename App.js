c959e87e36ee8f965172782c0c94f3f18a004908
Matheus Lima <matheusinclima@gmail.com>
2022-09-14 08:36:53 -0300

    activating trigger

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello worlds</Text>
+      <Text>Hello world</Text>
       <StatusBar style="auto" />
     </View>
   );
